e56e6943f20330c73ea6bee92c0465d699f278a7
Tim Franssen <mail@timendus.com>
2022-05-25 10:27:22 +0200

    Convert scss -> css

--- keuzetool/webpack.config.js
+++ keuzetool/webpack.config.js
@@ -2,10 +2,32 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: [
+        __dirname + '/src/index.js',
+        __dirname + '/scss/index.scss'
+    ],
   output: {
     path: path.resolve(__dirname),
     filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [],
+      }, {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: { outputPath: '/', name: '[name].min.css'}
+          },
+          'sass-loader'
+        ]
+      }
+    ]
   },
   devServer: {
     port: 9000,
