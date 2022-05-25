15d477ebb73f101aa9d0c85063a4eea9194ec4a0
Yannick Donners <yannick.donners@nedap.com>
2022-05-25 11:52:18 +0200

    Fix recursive search helper

--- keuzetool/src/helper.js
+++ keuzetool/src/helper.js
@@ -2,20 +2,35 @@
  * This method allows for recursively looking through a JSON tree and return the path
  * @param obj Object (json blob or child)
  * @param path Current path of the key
- * @param arr Something...
+ * @param arr Array to store all currently defined paths
  */
-function recurse(obj, path = '', arr = []) {
+function findSearchKeysRecurse(obj, path = '', arr = []) {
   for (let key in obj) {
     let item = obj[key]
-    let newPath = `${path}.${key}`
+    let newPath
 
-    if (typeof item == 'object') {
-      recurse(item, newPath, arr)
+    // Time for some magic: Check if we already have a path (to avoid a dot in front of the path) and if the key is 
+    // not a number. Then we can concat the current path with the new key. 
+    if (path.length > 0 && isNaN(parseInt(key))) {
+      newPath = `${path}.${key}`
+    // If however the key is a number, we can simply ignore this step and use the current path.
+    } else if (!isNaN(parseInt(key))) {
+      newPath = path
+    // In all other cases, we probably are dealing with the first entry and simply start with the key
     } else {
-      if (key !== 'id' || typeof key !== 'number') {
+      newPath = key
+    }
+
+    // If the item is an object, that means we can dive into that and find more paths!
+    if (typeof item == 'object') {
+      findSearchKeysRecurse(item, newPath, arr)
+    } else {
+      // We don't need to add `id` to the search keys
+      if (key !== 'id') {
         arr.push(newPath)
       }
     }
   }
-  return arr
+  // Make sure we only have unique entries in our array
+  return [...new Set(arr)]
 }
